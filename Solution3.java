33167821ec4e827af027664801c930941471a148
harshita rathore <harshitar713@gmail.com>
2026-06-18 16:52:00 +0530

    solution 3

---
+++ Solution3.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class Solution3{
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int lineNumber = 1;
+        
+        while(sc.hasNext()){
+        String line  = sc.nextLine();
+        System.out.println(lineNumber + " " + line);
+        lineNumber++;
+        }
+        
+        sc.close();
+    }
+}
