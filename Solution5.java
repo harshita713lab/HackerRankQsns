ff8e6c68492a4e864a7e63798f037f485deb8517
harshita rathore <harshitar713@gmail.com>
2026-07-01 00:45:32 +0530

    complete

---
+++ Solution5.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Solution5{
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        
+        try{
+            int n = sc.nextInt();
+            String s =  String.valueOf(n);
+            System.out.println("Good job"); 
+            if(s != null){
+                System.out.println("Good job");
+            }else{
+                System.err.println("Wrong answer");
+            }
+
+        }catch (Exception e){
+            System.out.println("Wrong answer");
+            
+        }
+    
+        
+    }
+}
