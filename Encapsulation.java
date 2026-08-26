59c1cd3af63748bbb52798a9cd92f41f24640e7a
harshita713lab <harshitar713@gmail.com>
2026-08-26 23:36:53 +0530

    learn this keyword

--- Encapsulation.java
+++ Encapsulation.java
@@ -3,15 +3,16 @@ class Human{
     private String name;
     private String FatherName;
 
-    void setAge(int a){
-        age = a;
+    void setAge(int age, Human obj){
+        Human obj1 = obj;
+        obj1.age = age;
     }
     int getAge(){
         return age;
     }
 
-    void setName(String n){
-        name = n;
+    void setName(String name){
+        this.name = name;
     }
     String getName(){
         return name;
@@ -27,11 +28,12 @@ class Human{
 public class Encapsulation{
     public static void main(String agrs[]){
         Human obj = new Human();
-        obj.setAge(31);
+        obj.setAge(31, obj);
         obj.setName("Dolly");
         obj.setFaterName("Trilok");
 
 
-        System.out.println(obj.getFaterName());
+        System.out.println(obj.getAge());
+        System.out.println(obj.getName());
     }
 }
