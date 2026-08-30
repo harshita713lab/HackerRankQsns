bb3f6c43d0cf2e9a3a2e922c374d8062bd830fb4
harshita713lab <harshitar713@gmail.com>
2026-08-30 23:54:30 +0530

    understand multi level inheritance

--- Demo.java
+++ Demo.java
@@ -1,10 +1,11 @@
 public class Demo{
     public static void main(String[] args) {
-        Inheritance2 obj = new Inheritance2();
+        MultiLevelInheritance obj = new MultiLevelInheritance();
         int r1 = obj.add(20, 10);
         int r2 = obj.sub(20, 10);
         int r3 = obj.divide(10, 2);
         int r4 = obj.mul(10, 3);
-        System.out.println(r1 + " : " + r2 + " : " + r3 + " : " + r4);
+        double r5 = obj.power(4, 2);
+        System.out.println(r1 + " : " + r2 + " : " + r3 + " : " + r4 + " : " + r5);
     }
 }
---
+++ MultiLevelInheritance.java
@@ -0,0 +1,5 @@
+public class MultiLevelInheritance extends Inheritance2{
+    public double power(int a , int b){
+        return Math.pow(a, b);
+    }
+}
