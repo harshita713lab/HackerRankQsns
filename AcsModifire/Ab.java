cb8926ad2dc8d16f84ddd7bbe90219459c01b168
harshita713lab <harshitar713@gmail.com>
2026-09-06 00:38:21 +0530

    add proetcted

--- AcsModifire/Ab.java
+++ AcsModifire/Ab.java
@@ -1,5 +1,18 @@
 package AcsModifire;
-public class Ab{
-    public int marks = 6;
+
+public class Ab {
+protected int marks = 6;
 }
 
+class Lonch {
+    public void abc() {
+        Ab obj = new Ab();
+        System.out.println(obj.marks);
+    }
+
+    public static void main(String[] args) {
+        Lonch l = new Lonch();
+        l.abc(); 
+    
+}
+}
